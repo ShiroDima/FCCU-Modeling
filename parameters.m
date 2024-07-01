29adecae59528f7c6c0ccdfa36179bfe64b18237
ShiroDima <sd.akarawak@gmail.com>
2024-07-01 01:40:05 +0100

    Changes made to the parameters file. All parameters for the vapourizer subsystem added.
    Vapourizer subsystem code all done

--- parameters.m
+++ parameters.m
@@ -5,21 +5,36 @@
 % This script loads or saves all the required parameters for the entire project
 % as a .dat file
 
-clc
+clc; clear;
 
-fileName = "parameters.mat" % Name of the file where parameters will be saved to and loaded from
+fileName = "parameters.mat"; % Name of the file where parameters will be saved to and loaded from
 
 % PARAMETERS BY SUBSYSTEM
 
-% VAPOURISER SUBSYSTEM
-cp_vgo_liquid = 9;
-cp_vgo_vapour = 10;
+% VAPOURIZER SUBSYSTEM
 
+% VGO component
+cp_vgo_liquid = 9; % Specific Heat Capacity of the VGO component in the liquid phase
+cp_vgo_vapour = 10; % Specific Heat Capacity of the VGO component in the vapour phase
+flowrate_vgo = 1000; % Flowrate of the VGO component
+temperature_vgo = 1000; % Temperature of the VGO component
+enthalpyVAP_vgo = 1000; % Enthalpy of vapourization of the VGO component
+temperature_vap = 1000; % Boiling point temperature at which the VGO component vapourizes
 
-% first check if the data file exists
+% Catalyst component
+flowrate_regen_catalyst = 1000; % Flowrate of the catalyst component
+cp_regen_catalyst = 1000; % Specific Heat Capacity of the catalyst component
+temperature_regen_catalyst = 1000; % Temperature of the catalyst component
+
+% Steam component
+flowrate_steam = 1000; % Flowrate of the steam component
+cp_steam = 4200; % Specific Heat Capacity of the steam component
+temperature_steam = 1000; % Temperature of the steam component
+
+% check if the data file exists
 if isfile(fileName)
     % File exists. Load it
-    clear
+    
     load(fileName)
 else
     % File does not exist, save parameters to file
--- subsystems/vapouriser.m
+++ subsystems/vapouriser.m
@@ -3,4 +3,32 @@
 % School: University of Lagos
 
 % Script for the Vapouriser subsystem of the FCCU
+% The vapouriser components are:
+% - The Vacuum Gas Oil (VGO)
+% - The Regenerated catalyst
+% - The Steam
+
+% This script solves an energy balance equation for the temperature of the feed into the reactor (temperature_in)
+
+% First check if parameters have been loaded into the MATLAB workspace
+fileName = "parameters.mat";
+
+if ~isfile(fileName)
+    % File was not found and the parameters.m file should be run to load parameters into workspace
+    parameters
+end
+
+% Numerator terms
+vgo_term_num = flowrate_vgo * ((cp_vgo_liquid * (temperature_vgo - temperature_vap)) + (cp_vgo_vapour * temperature_vap) - enthalpyVAP_vgo);
+cat_term_num = flowrate_regen_catalyst * cp_regen_catalyst * temperature_regen_catalyst;
+steam_term_num = flowrate_steam * cp_steam * temperature_steam;
+
+% Denominator terms
+vgo_term_den = flowrate_vgo * cp_vgo_vapour
+cat_term_den = flowrate_regen_catalyst * cp_regen_catalyst
+steam_term_den = flowrate_steam * cp_steam
+
+% Calculate the temperature of the feed into the riser reactor with the following equation
+temperature_in = (vgo_term_num + cat_term_num + steam_term_num) / (vgo_term_den + cat_term_den + steam_term_den);
+
 
